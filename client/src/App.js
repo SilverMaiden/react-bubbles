a9e6d16ab05813d68058a537d3868c6b0121f403
SilverMaiden <h.specials@gmail.com>
2019-12-20 09:02:49 -0800

    Corrected typo in axiosWithAuth header, created privateroute for bubblesPage component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+        {/*
+          Build a PrivateRoute component that will
+          display BubblePage when you're authenticated
         */}
+
+        <PrivateRoute exact path='/bubblepage' component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/axiosWithAuth.js
+++ client/src/axiosWithAuth.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
+
 export const axiosWithAuth = () => {
-    const token=localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
     return axios.create({
         baseURL: 'http://localhost:5000/',
         headers: {
-            'Content-Type': 'applicaition/json',
-            'Authorization' : token,
+            'Content-Type' : 'application/json',
+            'Authorization': token,
         }
     })
 }
